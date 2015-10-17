ddef2311962e300a1080d9718cf23f64fa806f20
Jenss21025 <jens_borgenon@hotmail.com>
2015-10-17 12:32:25 +0200

    Domain gemaakt door Jens

---
+++ TripPlanner/TripPlanner/MVVM/Model/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3_Data.MVVM.Model
+{
+    public class Item : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        private Boolean Is_Checked;
+
+        public int Priority
+        {
+            get { return Priority; }
+            set { Priority = value; NotifyPropertyChanged("Priority"); }
+        }
+
+        public Item(string naam, int priority)
+        {
+            Name = naam;
+            Priority = priority;
+            Is_Checked = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ TripPlanner/TripPlanner/MVVM/Model/List.cs
@@ -0,0 +1,27 @@
+﻿using Les3_Data.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripPlanner.MVVM.Model
+{
+    class List {
+        public List<Item> lijst;
+        public List()
+        {
+         lijst = new List<Item>();
+        }
+
+        public void Add_Item(Item item)
+        {
+            lijst.Add(item);
+        }
+
+        public void Delete_Item(Item item)
+        {
+            lijst.Remove(item);
+        }
+    }
+}
