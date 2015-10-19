dcd76e9a510d1e17a9958dbec0cbdcb704717391
Frederik De Smedt <frederik.de.smedt@hotmail.com>
2015-10-19 12:56:14 +0200

    Update Item.cs

--- TripPlanner/TripPlanner/MVVM/Model/Item.cs
+++ TripPlanner/TripPlanner/MVVM/Model/Item.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Les3_Data.MVVM.Model
+namespace TripPlanner.MVVM.Model
 {
     public class Item : INotifyPropertyChanged
     {
